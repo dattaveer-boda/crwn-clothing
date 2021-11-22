7a5e385548706a72665df6295633754cb686ec53
dattaveer-boda <dattaveer239@outlook.com>
2021-11-22 16:01:47 +0530

    sign up and sign in implemented with firebase auth

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,28 @@ const config = {
   measurementId: "G-7JE16KZGBZ",
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return false;
+  const userRef = firestore.doc(`/users/${userAuth.uid}`);
+  // (or) const userRef = firestore.collection('user').doc(userAuth.uid)
+  const snapShot = await userRef.get();
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+  return userRef;
+};
+
 firebase.default.initializeApp(config);
 
 export const auth = firebase.auth();
