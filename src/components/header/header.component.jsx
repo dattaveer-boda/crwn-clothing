11c5cde93c1d3a11398bb6520231aaa604c7c783
dattaveer-boda <dattaveer239@outlook.com>
2021-11-24 18:33:18 +0530

    added cart-icon cart-dropdown partial features

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,8 +6,10 @@ import { auth } from "../../firebase/firebase.utils";
 
 import "./header.styles.scss";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, isCartHidden }) => (
   <div className="header">
     <Link to="/" className="logo-container">
       <Logo className="logo" />
@@ -31,13 +33,16 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon />
     </div>
+    {isCartHidden ? null : <CartDropdown />}
   </div>
 );
 
 const mapStateToProps = (state) => {
   return {
     currentUser: state.user.currentUser,
+    isCartHidden: state.cart.isCartHidden,
   };
 };
 
